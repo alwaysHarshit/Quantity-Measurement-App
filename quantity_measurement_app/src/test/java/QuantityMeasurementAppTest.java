dcf936222ccc4e99ee13153faef6b1bbed4b00be
Harshit <studyharshit21@gmail.com>
2026-02-21 23:24:34 +0530

    solved UC2 reafactored

--- quantity_measurement_app/src/test/java/QuantityMeasurementAppTest.java
+++ quantity_measurement_app/src/test/java/QuantityMeasurementAppTest.java
@@ -5,51 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class QuantityMeasurementAppTest {
 
-    //testcase of UC-1
-    @Test
-    public void testFeetEquality_SameValue(){
-        Feet feet1 = new Feet(12.0);
-        Feet feet2 = new Feet(12.0);
-
-        assertTrue(feet1.equals(feet2));
-
-    }
-
-    @Test
-    public void testFeetEquality_DifferentValue(){
-        Feet feet1 = new Feet(12.0);
-        Feet feet2 = new Feet(13.0);
-
-        assertFalse(feet1.equals(feet2));
-
-    }
-
-    @Test
-    public void testFeetEquality_NullComparison(){
-        Feet feet1 = new Feet(12.0);
-        Feet feet2 = null;
-
-        assertFalse(feet1.equals(feet2));
-
-    }
-
-    @Test
-    public void testFeetEquality_DifferentClass(){
-        Feet feet1 = new Feet(12.0);
-        Double feet2 = 12.0;
-
-        assertFalse(feet1.equals(feet2));
-
-    }
-
-    @Test
-    public void testFeetEquality_SameReference(){
-        Feet feet1 = new Feet(12.0);
-
-        assertTrue(feet1.equals(feet1));
-
-    }
-
    //test cases of UC 2
 
     @Test
